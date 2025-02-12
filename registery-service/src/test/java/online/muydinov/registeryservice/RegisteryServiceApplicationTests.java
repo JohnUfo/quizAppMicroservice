df99c67d844c9893682c8ad00b9c1db26011da9f
JohnUfo <tursunov.umidjon.uz@gmail.com>
2025-02-12 16:00:18 +0500

    registery service added:

---
+++ registery-service/src/test/java/online/muydinov/registeryservice/RegisteryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package online.muydinov.registeryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegisteryServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
