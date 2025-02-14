ea224ac147d27f1c5b71423df96d830b4c84575c
JohnUfo <tursunov.umidjon.uz@gmail.com>
2025-02-14 21:17:50 +0500

    Deleted QuizletClone from repository

--- QuizletClone/src/main/java/online/muydinov/quizletclone/repository/FlashCardSetRepository.java
+++
@@ -1,7 +0,0 @@
-package online.muydinov.quizletclone.repository;
-
-import online.muydinov.quizletclone.entity.FlashcardSet;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface FlashCardSetRepository extends JpaRepository<FlashcardSet, Long> {
-}
--- QuizletClone/src/main/java/online/muydinov/quizletclone/service/UserService.java
+++
@@ -1,9 +0,0 @@
-package online.muydinov.quizletclone.service;
-
-import org.springframework.stereotype.Service;
-
-@Service
-public class UserService {
-
-
-}
